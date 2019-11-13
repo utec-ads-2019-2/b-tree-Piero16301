5df1df0ea39928e6a2f394b05cc274bcab327831
Piero Angelo <piero.morales@utec.edu.pe>
2019-11-13 18:30:22 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Arbol_B.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Arbol_B.exe"
+  "Arbol_B.exe.manifest"
+  "Arbol_B.pdb"
+  "CMakeFiles/Arbol_B.dir/main.cpp.obj"
+  "libArbol_B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Arbol_B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Arbol_B.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Arbol_B.
+# This may be replaced when dependencies are built.
